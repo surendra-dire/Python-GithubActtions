7442b12cce0d271157217682889d847cbaea8d92
surendradire <surendra.dire@gmail.com>
2025-03-25 16:29:27 +0100

    update project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="your-python-app",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        # List of dependencies
+        'requests',
+    ],
+)
